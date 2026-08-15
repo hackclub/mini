14e4cd9d0ad296c83cf01bd471744edd2e888919
sahana <github@sahana.dev>
2026-08-15 14:21:10 -0400

    og:image

--- astro.config.ts
+++ astro.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://mini.hackclub.com',
     fonts: [
         {
             provider: fontProviders.google(),
