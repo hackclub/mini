87dc2421ce09eab6bbf711f0f5fc7db7982cf9b5
sahana <github@sahana.dev>
2026-08-15 11:34:18 -0400

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+# mini
+
+This is the site I made for the 2026 YSWS Summer Interns application.
+
+The site is deployed at [512.sahana.dev](https://512.sahana.dev)!
+
+TLDR: 
+* You ship: a tiny website, PCB, game, or other constrained project
+* We ship: smol prizes, including stickers, components, and pocket-sized consoles
+
+## Development
+
+This site is made using Astro, Typescript and Bun. You can build/deploy it using `bun build && bun start`. You can start a development server `bun dev`
---
+++ astro.config.ts
@@ -0,0 +1,45 @@
+// @ts-check
+import { defineConfig, fontProviders } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite'
+
+// https://astro.build/config
+export default defineConfig({
+    fonts: [
+        {
+            provider: fontProviders.google(),
+            name: 'Nunito Sans',
+            cssVariable: '--font-nunito-sans',
+            weights: [400, 700, 800, 900],
+            styles: ['normal'],
+            subsets: ['latin'],
+            formats: ['woff2'],
+            fallbacks: ['sans-serif'],
+        },
+        {
+            provider: fontProviders.google(),
+            name: 'Darumadrop One',
+            cssVariable: '--font-darumadrop-one',
+            weights: [400],
+            styles: ['normal'],
+            subsets: ['latin'],
+            formats: ['woff2'],
+            fallbacks: ['serif'],
+        },
+        {
+            provider: fontProviders.google(),
+            name: 'Pixelify Sans',
+            cssVariable: '--font-pixelify-sans',
+            weights: [400],
+            styles: ['normal'],
+            subsets: ['latin'],
+            formats: ['woff2'],
+            fallbacks: ['monospace'],
+        },
+    ],
+    vite: {
+        plugins: [tailwindcss()],
+        server: {
+            allowedHosts: ['.ngrok-free.dev', '.trycloudflare.com'],
+        },
+    },
+});
